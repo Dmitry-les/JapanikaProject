e3e4c572f4749e024c0989a7385e6bd76bad142f
User <User@DESKTOP-V6QH7CG>
2021-09-08 15:22:18 +0300

    New2

---
+++ src/test/java/Japanika/ExtentManager.java
@@ -0,0 +1,89 @@
+package Japanika;
+
+import java.awt.AWTException;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.google.common.io.Files;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+	
+	private WebDriver driver;
+
+	public ExtentManager(WebDriver driver){               
+	        this.driver = driver;     
+	    }
+	private static ExtentReports extent;
+	private static ExtentTest test;
+	private static ExtentSparkReporter htmlReporter;
+		
+	static DateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+	static Date today = Calendar.getInstance().getTime(); 
+	static String reportDate = df.format(today);
+	public static String filePath = "C:\\Users\\User\\Desktop\\Dima\\Japanika\\TestReport\\JapanikaProject " +reportDate+ "\\exReport.html";
+		
+	public static ExtentReports GetExtent(){
+		new File("C:\\Users\\User\\Desktop\\Dima\\Japanika\\TestReport\\JapanikaProject "+ reportDate).mkdirs();
+		if (extent != null)
+            return extent; //avoid creating new instance of html file
+            extent = new ExtentReports();		
+		extent.attachReporter(getHtmlReporter());
+		return extent;
+	}
+		
+	private static ExtentSparkReporter getHtmlReporter() {
+		
+        htmlReporter = new ExtentSparkReporter(filePath);
+		
+	  // make the charts visible on report open
+      //  htmlReporter.config().setChartVisibilityOnOpen(true);
+      //  htmlReporter.loadXMLConfig("C:\\selenium\\jars\\extentreports-java-3.1.5/extent-config.xml");
+        htmlReporter.config().setDocumentTitle("JapanikaQATest");
+        htmlReporter.config().setReportName("Regression cycle");
+        htmlReporter.config().setEncoding("utf-8"); 
+        return htmlReporter;
+	}
+		
+	public static ExtentTest createTest(String name, String description){
+		test = extent.createTest(name, description);
+		return test;
+	}
+	
+	public static String CaptureScreen(WebDriver driver) throws AWTException, IOException
+	{
+		LocalDateTime now = LocalDateTime.now();
+		String time =now.format(DateTimeFormatter.ofPattern("ddHHmmss", Locale.ENGLISH)); 		
+		String folderPath = ("C:\\Users\\User\\Desktop\\Dima\\Japanika\\TestReport\\JapanikaProject " + reportDate);
+		String imagePath   = folderPath  +"/pic" +time +".jpg"; 
+		
+		TakesScreenshot oScn = (TakesScreenshot) driver;
+		File oScnShot = oScn.getScreenshotAs(OutputType.FILE);
+		File oDest = new File(imagePath);
+
+		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);  
+		 Files.copy(scrFile, oDest);
+
+//		Robot robot = new Robot();
+//		BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+//		ImageIO.write(screenShot, "JPG", new File(ImagesPath));
+//		
+		
+		return imagePath;
+	}
+}
+
+
--- src/test/java/Japanika/JapanikaRun.java
+++ src/test/java/Japanika/JapanikaRun.java
@@ -1,42 +1,69 @@
 package Japanika;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Ignore;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class JapanikaRun {
 	
+	static WebDriver driver;
+	static PomJapanika pom;
+	static ExtentManager Jap = new ExtentManager(driver);
+	static ExtentReports extent;
+	static ExtentTest test1;
+//	static ExtentTest test2;
+//	static ExtentTest test3;
+//	static ExtentTest test4;
 	
-	
-@Test
-  public void f() {
-  }
-  @BeforeMethod
-  public void beforeMethod() {
-  }
-
-  @AfterMethod
-  public void afterMethod() {
-  }
-
   @BeforeClass
-  public void beforeClass() {
+  public static void beforeClass() {
+	  
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\Dima//chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.japanika.net/");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+		
+		pom = new PomJapanika();
+		pom = PageFactory.initElements(driver, PomJapanika.class);
+		
+		extent = Jap.GetExtent();
+		test1 = Jap.createTest("test1", "ChatOnLine");
   }
 
   @AfterClass
-  public void afterClass() {
+  public static void afterClass() {
+		extent.flush();
+		driver.quit();
   }
+  @Ignore
+  @Test
+  public void test1() throws InterruptedException {
+	  pom.ChatOnLine.click();
+	  
+	  System.out.println(driver.getTitle());	
 
-  @BeforeTest
-  public void beforeTest() {
+	  if (driver.getTitle().equals("בית - Japanika")) {
+		  test1.fail("ChatOnLine - test fail");	
+	  }else {
+		  test1.pass("ChatOnLine - test pass");
+	  }
+	  Thread.sleep(2000);
+	 
+	  
   }
-
-  @AfterTest
-  public void afterTest() {
-  }
+  
 
 }
